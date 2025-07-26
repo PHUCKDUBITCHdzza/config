ec9678de032a361ec684f71c3aa75e019916208e
DuongLucPhi <cholacoho123456789@gmail.com>
2025-07-26 22:12:53 +0700

    Update marmot config.lua

--- marmot config.lua
+++ marmot config.lua
@@ -4,7 +4,7 @@ getgenv().gagConfig = {
     -- Event:
     CRAFT_EVENT = { "Anti Bee Egg" },
     BUY_TRAVELING_MERCHANT = { "Bee Egg"},
-    BUY_EVENT_SHOP = { "Zen Egg", "Koi"},
+    BUY_EVENT_SHOP = { "Zen Egg", ["Koi"] = 22, ["Raiju"] = 1},
     MAX_EVENT_RESTOCK_SHECKLES = 1000000000,
     
     -- General:
