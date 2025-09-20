43a255eb658df60fb7eeab217a25e5de4b928c56
PhockYouBitch <cholacoho123456789@gmail.com>
2025-09-21 01:14:44 +0700

    Update and rename marmot config.lua to received.lua

---
+++ received.lua
@@ -0,0 +1,11 @@
+script_key="oBpacYiqvVNEYrruawLMLMPNANOfZWpk";
+
+getgenv().gagConfig = {
+    LOW_CPU = true,  -- Tralalero Tralala
+    KICK_AFTER_GIFT_PET = false,  -- Kick after all GIFT_PET pets gifted
+    AUTO_COLLECT = true,  -- Auto collect ALL fruit & pets
+    GIFT_USERNAME = {  },  -- Username to gift
+    GIFT_PET = { "Barn Owl" },  -- Pet name to gift
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/85b3f7e4c867c468617e5c93fa3a818d.lua"))()
