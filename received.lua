f707135b9fa004588f82f6bcab39118b729dd96e
PhockYouBitch <cholacoho123456789@gmail.com>
2025-09-21 01:35:02 +0700

    Update received.lua

--- received.lua
+++ received.lua
@@ -4,8 +4,42 @@ getgenv().gagConfig = {
     LOW_CPU = true,  -- Tralalero Tralala
     KICK_AFTER_GIFT_PET = false,  -- Kick after all GIFT_PET pets gifted
     AUTO_COLLECT = true,  -- Auto collect ALL fruit & pets
-    GIFT_USERNAME = {  },  -- Username to gift
+    GIFT_USERNAME = {
+"X9Zz0BAedT",
+"jHMXDFWJPj",
+"U3jNmGXTm7",
+"0xtsmpW7Vm",
+"9D51TVkBe4",
+"3MBG7D5ArN",
+"KT05yjxJJW",
+"jBPFvBRiSY",
+"mfJmklFpvF",
+"ndQNxOPMZT",
+"M6oSfmZgzU",
+"K869Yrkztq",
+"RXxAyXroG9",
+"bY0IpMtAVr",
+"1x2SccrhrK",
+"QE64i6nTBp",
+"ItAirPwGZ0",
+"TVPJzvuUE6",
+"2SKlgefXPJ",
+"Ee6KTCi8ni",
+"iNIun1oxyW",
+"MOh2TBuvaX",
+"nGTEeVBOFz",
+"gkE2JjKTlZ",
+"FYeBBMWMhx",
+"rMVcGfF4mQ",
+"IIFoO3Kd3Q",
+"5W4DGGHUpX",
+"12lo9b8uwm",
+"hBsPzDktHi",
+"5AXsOOJiRa",
+"FoNq7yD4dP",
+    },  -- Username to gift
     GIFT_PET = { "Barn Owl" },  -- Pet name to gift
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/85b3f7e4c867c468617e5c93fa3a818d.lua"))()
+
