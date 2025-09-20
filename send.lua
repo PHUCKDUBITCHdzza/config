39cf3593eefedece1588a514801fe920709f64fb
PhockYouBitch <cholacoho123456789@gmail.com>
2025-09-21 01:15:27 +0700

    Update and rename ayaya config to send.lua

---
+++ send.lua
@@ -0,0 +1,64 @@
+script_key="oBpacYiqvVNEYrruawLMLMPNANOfZWpk";
+
+getgenv().gagConfig = {
+    LOW_CPU = true,  -- Tralalero Tralala
+    KICK_AFTER_GIFT_PET = true,  -- Kick after all GIFT_PET pets gifted
+    AUTO_COLLECT = false,  -- Auto collect ALL fruit & pets
+    GIFT_USERNAME = { 
+"99iDW6peCD",
+"4HHCXJU2Eb",
+"oN7KTTqC62",
+"9ynP69wmM0",
+"k1rCadTiJq",
+"EIvhnp9cEW",
+"HFxcye6DMy",
+"jbdt1yrN2P",
+"Ohn1yofMO5",
+"HMs03ajZdj",
+"sVIIoB6WZ9",
+"lgVBvo2X6o",
+"6WuiSLG0tD",
+"k9WcyA4x42",
+"jGUolLadKK",
+"YANLeto4vt",
+"H2d1nrAsjq",
+"WEwWkiuaoY",
+"Z7B47ITWQl",
+"00jeTMXxVZ",
+"X9Zz0BAedT",
+"jHMXDFWJPj",
+"U3jNmGXTm7",
+"0xtsmpW7Vm",
+"9D51TVkBe4",
+"3MBG7D5ArN",
+"KT05yjxJJW",
+"jBPFvBRiSY",
+"mfJmklFpvF",
+"ndQNxOPMZT",
+"M6oSfmZgzU",
+"K869Yrkztq",
+"RXxAyXroG9",
+"bY0IpMtAVr",
+"1x2SccrhrK",
+"QE64i6nTBp",
+"ItAirPwGZ0",
+"TVPJzvuUE6",
+"2SKlgefXPJ",
+"Ee6KTCi8ni",
+"iNIun1oxyW",
+"MOh2TBuvaX",
+"nGTEeVBOFz",
+"gkE2JjKTlZ",
+"FYeBBMWMhx",
+"rMVcGfF4mQ",
+"IIFoO3Kd3Q",
+"5W4DGGHUpX",
+"12lo9b8uwm",
+"hBsPzDktHi",
+"5AXsOOJiRa",
+"FoNq7yD4dP",
+},  -- Username to gift
+    GIFT_PET = { "Barn Owl" },  -- Pet name to gift
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/85b3f7e4c867c468617e5c93fa3a818d.lua"))()
